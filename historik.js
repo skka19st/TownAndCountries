b5ea7e36cc398c783c86603d879414905dbe6238
Karin Skans <skka19st@student.ju.se>
2019-10-04 11:39:05 +0200

    klar fÃor inlamning

--- historik.js
+++ historik.js
@@ -1,8 +1,6 @@
 // Javascript-dokument 
 
 // hämta tidigare besökta städer från local Storage
-console.log("start av Histrikstäder:");
-
 var stadstring = localStorage.getItem("stad");
 
 if (stadstring === null) {  
@@ -16,30 +14,23 @@ if (stadstring === null) {
 // rensa tidigare historik, uppdatera sidan
 function RensaHistorik(){
     localStorage.removeItem("stad");
-    console.log ("efter rensning " + localStorage);
     document.getElementById("historikrad").innerHTML = "";
 }
 
 function ByggHistorikSida(stadfil, stadlista) {
-    console.log("funktionen ByggHistorikSida:");
-    console.log("stadlista = " + stadlista);
-
     // rensa skärmen från ev gammalt data
     document.getElementById("historikrad").innerHTML = "";
-    //document.getElementById("valHist").innerHTML = "";
     
     // hämta fakta för de städer som ligger lagrade
     var arrayInd = 0;
     var totAntal = 0;
     for (stadInd = 0 ; stadInd < stadfil.length ; stadInd++) {
-        console.log("stadInd = " + stadInd + stadfil[stadInd].id);
-        console.log("ArrayInd = " + arrayInd + stadlista[arrayInd]);
         var num1 = Number(stadfil[stadInd].id);
         var num2 = Number(stadlista[arrayInd]);
-        if (num1 === num2) {
+
         //if (stadfil[stadInd].id === stadlista[arrayInd]) {
+        if (num1 === num2) {
             // stadens namn till detaljraden
-            console.log("träff!");
             kodtext = "";
             var namn = stadfil[stadInd].stadname;
             kodtext += '<div id="namn">' + namn + '</div>';
@@ -63,12 +54,8 @@ function ByggHistorikSida(stadfil, stadlista) {
     var listradtext = document.getElementById("historikrad");
     listradtext.insertAdjacentHTML("beforeend", kodtext)
 
-    // tillbaka eller rensa historik (bottons) 
+    // rensa historik (botton) 
     var kodtext = "";
-    // var ledtext = "Tillbaka till förstasidan";
-    // kodtext += '<button onClick="Tillbaka()">';
-    // kodtext += ledtext + '</button>';
-
     var ledtext = "Rensa historik";
     kodtext += '<button onClick="RensaHistorik()">';
     kodtext += ledtext + '</button>';
@@ -81,18 +68,22 @@ function ByggHistorikSida(stadfil, stadlista) {
 // inläsning av stad-filen
 function StadfilIn(stadlista){
     fetch ("stad.json")
+
     // '.then' är ett promise som gör att JavaScript väntar tills 
     // förra anropet är klart
+    // 'stadfil' är resultatet av fetch-läsningen 
     .then (function(stadfil) { 
         // return skickar resultatet vidare till nästa '.then' 
-        // 'stadfil.json()' är resultatet av fetch-läsningen          
+        // 'stadfil.json()' returnerar filen i json-format         
         return stadfil.json();                 
     })
+
     // 'stadfil' innehåller resultatet av föregående '.then'
     // function utan namn (triggas av en händelse, anropas aldrig utifrån)
     .then (function(stadfil) {  
         ByggHistorikSida(stadfil, stadlista);                
     })
+
     // '.catch' hanterar fel
     .catch(function(staderror) {
         console.log("fel vid inläsning av stad");
